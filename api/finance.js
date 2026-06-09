668ac61cbf96b380ec73ce8a23dd0a01d8315df3
Justin Do <justin.do@sap.com>
2026-06-09 08:47:40 -0400

    Fix syntax error in finance.js: restore missing module.exports = async line lost during sparkline insertion

--- api/finance.js
+++ api/finance.js
@@ -399,7 +399,7 @@ async function handleSparkline(req, res) {
   return res.status(200).json(data);
 }
 
-
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
